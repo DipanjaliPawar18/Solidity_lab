7772ab3630fda8f5a16675d58ec889fda789eca9
Dipanjali <122343977+DipanjaliPawar18@users.noreply.github.com>
2024-09-15 21:13:21 +0530

    Add files via upload
    Factorial calculation

---
+++ fact.sol
@@ -0,0 +1,21 @@
+/* Write a solidity program to find factorial of a number */
+
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Factorial {
+    function factorial(uint256 number) public pure returns (uint256) {
+        if (number == 0 || number == 1) {
+            return 1;
+        } else {
+            uint256 fact = 1;
+
+            for (uint256 i = 2; i <= number; i++) {
+                fact *= i;
+                //result = result * i
+            }
+            return fact; //6
+        }
+    }
+}
